91cc2cf72b6cb9311283873a2b2c22f8ce616edd
HuangShen <1426887150@qq.com>
2018-11-05 19:16:10 +0800

    开始

---
+++ src/main/java/com/liushao/guangli/GuangliApplication.java
@@ -0,0 +1,12 @@
+package com.liushao.guangli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuangliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuangliApplication.class, args);
+    }
+}
---
+++ src/test/java/com/liushao/guangli/GuangliApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liushao.guangli;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuangliApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
